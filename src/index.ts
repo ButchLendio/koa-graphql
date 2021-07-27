70428f84d2ced68df9d9b72648bf238debff8228
butchlendio <butchlendio12@gmail.com>
2021-07-27 12:42:38 +0800

    feat: add private directives

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,63 @@ import Koa from "koa";
 import { MongooseService } from "./config/mongo";
 import { typeDefs } from "./schemas/typeDefs";
 import { resolvers } from "./resolvers/resolver";
+import { GraphQLSchema, defaultFieldResolver } from 'graphql';
+import { getDirectives, MapperKind, mapSchema } from '@graphql-tools/utils';
+import { AuthenticationError } from 'apollo-server-errors';
+import { Context } from 'koa';
+
+function privateDirectiveTransformer(schema: GraphQLSchema) {
+  const typeDirectiveArgumentMaps: Record<string, any> = {};
+  const directiveName = 'private';
+
+  return mapSchema(schema, {
+    [MapperKind.TYPE]: (type) => {
+      const typeDirectives = getDirectives(schema, type);
+      typeDirectiveArgumentMaps[type.name] = typeDirectives[directiveName];
+
+      return undefined;
+    },
+    [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
+      const fieldDirectives = getDirectives(schema, fieldConfig);
+      const directiveArgumentMap =
+        fieldDirectives[directiveName] ?? typeDirectiveArgumentMaps[typeName];
+
+      if (directiveArgumentMap) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        fieldConfig.resolve = async function (
+          source,
+          args,
+          context: Context,
+          info,
+        ) {
+          const [type, token] = context.get('Authorization').split(' ');
+
+          if (!type || type.toLowerCase() !== 'bearer' || !token) {
+            throw new AuthenticationError('Invalid authentication header.');
+          }
+
+          // Verify token and retrieve user information
+          // Add user in context
+
+          const user = { id: 'userId' };
+
+          return resolve(
+            source,
+            args,
+            {
+              ...context,
+              user,
+            },
+            info,
+          );
+        };
+
+        return fieldConfig;
+      }
+    },
+  });
+}
+
 
 const app = new Koa();
 
