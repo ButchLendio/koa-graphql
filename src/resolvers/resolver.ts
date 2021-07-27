a57fd2fab46fbcf68946e9f9771985f78810f9a0
butchlendio <butchlendio12@gmail.com>
2021-07-27 10:56:02 +0800

    feat: add authenticate

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -11,6 +11,7 @@ export const resolvers = {
       return "WEW";
     },
   },
+
   Mutation: {
     signUp: async (_: never, { input }) => {
       const { emailAddress, firstname, lastname, password } = input;
@@ -47,6 +48,39 @@ export const resolvers = {
       );
 
       return { token };
+    },
+
+    authenticate: async (_: never, { input }) => {
+      const { emailAddress, password } = input;
+
+      const foundUser = await Users.findOne({ emailAddress });
+      if (!foundUser) {
+        throw new UserInputError("User not registerd");
+      }
+
+      const passwordIsValid = await Bcryptjs.compare(password, foundUser.password);
+
+      if (passwordIsValid) {
+        const timeInMilliseconds = new Date().getTime();
+        const expirationTime =
+          timeInMilliseconds + Number(Token.expireTime) * 10_000;
+        const expireTimeInSeconds = Math.floor(expirationTime / 1_000);
+
+        const token = await Jwt.sign(
+          {
+            id:foundUser._id
+          },
+          Token.secret,
+          {
+            issuer: Token.issUser,
+            algorithm: "HS256",
+            expiresIn: expireTimeInSeconds,
+          }
+        );
+        return { token };
+      } else {
+        throw new UserInputError("Unauthorized");
+      }
     },
   },
 };
--- tests/users/users.spec.ts
+++ tests/users/users.spec.ts
@@ -1,11 +1,15 @@
 import Request from "supertest";
+import Bcryptjs from "bcryptjs";
 import { expect } from "chai";
 import { startServer } from "../../src/index";
 import Users from "../../src/models/users";
 import { generateFakeUser } from "../helpers/helpers";
+import { generateId, EntityType } from "../../src/schemas/generate-ids";
+
+
 
 describe("User Test", () => {
-  afterEach(async function () {
+  after(async function () {
     await Users.deleteMany({});
   });
 
@@ -23,10 +27,104 @@ describe("User Test", () => {
       .send({
         query: signUpMutation,
         variables: {
-          input:generateFakeUser(),
+          input: generateFakeUser(),
         },
       });
 
     expect(res.statusCode).to.equal(200);
+  });
+
+  it("authenticate", async function () {
+    const userCreate = generateFakeUser();
+
+    await Users.create({
+      ...userCreate,
+      id:generateId(EntityType.Account),
+      password: await Bcryptjs.hash(userCreate.password, 10),
+    });
+    const authenticatepMutation = `
+            mutation($input:AuthenticateInput!){
+              authenticate(input: $input){
+                    token
+                }
+            }
+        `;
+
+    const res = await Request(startServer)
+      .post("/graphql")
+      .send({
+        query: authenticatepMutation,
+        variables: {
+          input: {
+            emailAddress:userCreate.emailAddress,
+            password:userCreate.password
+          }
+        },
+      });
+    expect(res.statusCode).to.equal(200);
+  });
+
+  it("Unauthorized", async function () {
+    const userCreate = generateFakeUser();
+
+    await Users.create({
+      ...userCreate,
+      id:generateId(EntityType.Account),
+      password: await Bcryptjs.hash(userCreate.password, 10),
+    });
+    const authenticatepMutation = `
+            mutation($input:AuthenticateInput!){
+              authenticate(input: $input){
+                    token
+                }
+            }
+        `;
+
+    const res = await Request(startServer)
+      .post("/graphql")
+      .send({
+        query: authenticatepMutation,
+        variables: {
+          input: {
+            emailAddress:userCreate.emailAddress,
+            password:"lkajsdf"
+          }
+        },
+      });
+
+      const body =JSON.parse(res.text)
+    expect(body.errors[0].message).to.equal("Unauthorized");
+  });
+
+  it("Unauthorized", async function () {
+    const userCreate = generateFakeUser();
+
+    await Users.create({
+      ...userCreate,
+      id:generateId(EntityType.Account),
+      password: await Bcryptjs.hash(userCreate.password, 10),
+    });
+    const authenticatepMutation = `
+            mutation($input:AuthenticateInput!){
+              authenticate(input: $input){
+                    token
+                }
+            }
+        `;
+
+    const res = await Request(startServer)
+      .post("/graphql")
+      .send({
+        query: authenticatepMutation,
+        variables: {
+          input: {
+            emailAddress:"kasjhdfkjashfda",
+            password:userCreate.password
+          }
+        },
+      });
+
+      const body =JSON.parse(res.text)
+    expect(body.errors[0].message).to.equal("User not registerd");
   });
 });
