239acf773dd9d8e4d001c81facee6e04e1b25e29
butchlendio <butchlendio12@gmail.com>
2021-07-29 20:25:54 +0800

    test: add test for me

--- tests/users/me.spec.ts
+++ tests/users/me.spec.ts
@@ -5,9 +5,9 @@ import Users from "../../src/models/users";
 import { addFakeUserRegister, getToken } from "../helpers/helpers";
 import { generateId, EntityType } from "../../src/schemas/generate-ids";
 
-describe("Mutation.signUp", () => {
+describe("Mutation.me", () => {
   after(async function () {
-    // await Users.deleteMany({});
+    await Users.deleteMany({});
   });
   const meQuery = `
             query{
@@ -19,11 +19,10 @@ describe("Mutation.signUp", () => {
             }
         `;
 
-  it.only("should display account", async function () {
+  it("should display account", async function () {
     const ownerId = generateId(EntityType.Account);
 
     const user = await addFakeUserRegister({ ownerId });
-    console.log(user)
     const token = await getToken({ ownerId });
 
     const { body } = await Request(startServer)
@@ -33,8 +32,17 @@ describe("Mutation.signUp", () => {
       })
       .set("Authorization", `Bearer ${token}`);
 
-    console.log(body);
+    expect(body.data.me.emailAddress).to.equal(user.emailAddress);
+  });
 
-    // expect(res.statusCode).to.equal(200);
+  it("should error if no token", async function () {
+
+    const { body } = await Request(startServer)
+      .post("/graphql")
+      .send({
+        query: meQuery,
+      })
+
+    expect(body.errors[0].message).to.equal("Invalid authentication header.");
   });
 });
